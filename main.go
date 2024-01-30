d6afdae5bf43f9b04b117c49ec404d9aa63e6500
d1mpi <d1mpi.developer@gmail.com>
2024-01-30 19:03:44 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "context"
+	_ "flag"
+	_ "net/http"
+
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	_ "google.golang.org/grpc"
+	_ "google.golang.org/grpc/credentials/insecure"
+	_ "google.golang.org/grpc/grpclog"
+)
+
+func main() {
+
+}
